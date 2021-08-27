49709b3d3ea9c53af4ec31cc14f3d9feed04aff6
Uğur Utku Seyfeli <utkuseyfeli@gmail.com>
2021-08-27 13:20:39 +0300

    remote will be opened

---
+++ README.md
@@ -0,0 +1,6 @@
+Node version 16.8.0
+first deploy the commands by => node deploy-commands.js,
+then run the program => node index.js
+
+Important Note: You need to include your config.json file
+which has your token in it.
--- src/index.js
+++ src/index.js
@@ -1,38 +1,116 @@
-const fs = require('fs');
-const { Client, Collection, Intents, Channel } = require('discord.js');
-const { token } = require('../config.json');
-// const { channel } = require('diagnostics_channel');
-const { joinVoiceChannel } = require('@discordjs/voice');
+// const fs = require('fs');
+// const { Client, Collection, Intents, Channel } = require('discord.js');
+ const { token } = require('../config.json');
+// // const { channel } = require('diagnostics_channel');
+// const { joinVoiceChannel } = require('@discordjs/voice');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// client.commands = new Collection();
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const channel = new Channel(client);
+// const channel = new Channel(client);
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	client.commands.set(command.data.name, command);
+// }
+
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const command = client.commands.get(interaction.commandName);
+
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
+
+// client.login(token);
+
+
+const { Client, VoiceChannel, Intents } = require ('discord.js');
+const {
+	joinVoiceChannel,
+	createAudioPlayer,
+	createAudioResource,
+	entersState,
+	StreamType,
+	AudioPlayerStatus,
+	VoiceConnectionStatus,
+} = require( '@discordjs/voice' );
+
+const player = createAudioPlayer();
+
+function playSong() {
+	const resource = createAudioResource('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3', {
+		inputType: StreamType.Arbitrary,
+	});
+
+	player.play(resource);
+
+	return entersState(player, AudioPlayerStatus.Playing, 5e3);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
+async function connectToChannel(channel) {
+	const connection = joinVoiceChannel({
+		channelId: channel.id,
+		guildId: channel.guild.id,
+		adapterCreator: channel.guild.voiceAdapterCreator,
+	});
 
 	try {
-		await command.execute(interaction);
+		await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+		return connection;
+	} catch (error) {
+		connection.destroy();
+		throw error;
+	}
+}
+
+const client = new Client(
+	{ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] },
+);
+
+client.login(token);
+
+client.on('ready', async () => {
+	console.log('Discord.js client is ready!');
+
+	try {
+		await playSong();
+		console.log('Song is ready to play!');
 	} catch (error) {
 		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
-client.login(token);
+client.on('message', async (message) => {
+	if (!message.guild) return;
+
+	if (message.content === '-join') {
+		const channel = message.member?.voice.channel;
+
+		if (channel) {
+			try {
+				const connection = await connectToChannel(channel);
+				connection.subscribe(player);
+				message.reply('Playing now!');
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+			message.reply('Join a voice channel then try again!');
+		}
+	}
+});
+
